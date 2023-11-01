524be7a48705cd694ab6a2e54eae47c322be066f
SpaceCheetah <space-cheetah@hotmail.com>
2023-10-31 22:56:47 -0700

    Add converter between hex number and output

---
+++ ViewModels/Converters/HexConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace DIGIOController.ViewModels.Converters; 
+
+public class HexConverter : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is not string text || targetType != typeof(decimal?))
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        return int.TryParse(text, NumberStyles.HexNumber, null, out int result) ? (decimal)result : 0;
+    }
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (targetType != typeof(string) || (value is not decimal && value is not null)) {
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        }
+        return value is null ? "0" : $"{decimal.ToInt32((decimal)value):X}";
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using DIGIOController.Models;
+using ReactiveUI;
+using System;
 using System.Linq;
 
 namespace DIGIOController.ViewModels;
@@ -6,4 +8,31 @@ namespace DIGIOController.ViewModels;
 public class MainWindowViewModel : ViewModelBase {
     public Bit[] Inputs { get; } = Enumerable.Range(0, 8).Select(i => 7 - i).Select(i => new Bit(i)).ToArray();
     public Bit[] Outputs { get; } = Enumerable.Range(0, 8).Select(i => 7 - i).Select(i => new Bit(i)).ToArray();
+
+    static int Normalize(int value) => value & 0xFF;
+    
+    int _outputCombined = 0;
+    public int? OutputCombined {
+        get => _outputCombined;
+        set => this.RaiseAndSetIfChanged(ref _outputCombined, Normalize(value ?? 0));
+    }
+
+    public MainWindowViewModel() {
+        this.WhenAnyValue(x => x.OutputCombined).Subscribe(output => {
+            for (int i = Outputs.Length - 1; i >= 0; i--) {
+                Outputs[i].Set = (output & 1) == 1;
+                output >>= 1;
+            }
+        });
+        foreach (Bit b in Outputs) {
+            int mask = 1 << b.Position;
+            b.WhenAnyValue(x => x.Set).Subscribe(bit => {
+                if (bit) {
+                    OutputCombined |= mask;
+                } else {
+                    OutputCombined &= ~mask;
+                }
+            });
+        }
+    }
 }
